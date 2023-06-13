de848dc1815adc50a5e2527180ded65d68e458d9
Aufer Victoriano <aufer.vicc@gmail.com>
2023-06-12 23:34:00 -0500

    cors configurado para permitir todas las entradas

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 // middlewares
 app.use((0, morgan_1.default)('dev'));
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: '*'
+}));
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
 app.use('/', routes_1.default);
